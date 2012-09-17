9174fc0c1a677db677d66ee86c22195991765d7c
Neil Williams <neil@spladug.net>
2012-09-17 11:49:05 -0700

    setup.sh: Switch to apt-get for installing debian packages.

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ case $OSTYPE in
         fi
 
         # install the prereqs
-        sudo aptitude install $VIMPACKAGE make ruby-dev rake 
+        sudo apt-get install $VIMPACKAGE make ruby-dev rake 
         ;;
     darwin*)
         READLINK=greadlink
