9db2d01c2d40372b042bffe3dc2b2a0c23bea7f0
Neil Williams <neil@reddit.com>
2013-12-22 19:24:41 -0800

    Use dmenu instead of gnome-panel's launcher.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,8 @@ import XMonad.Config.Gnome
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.NoBorders
 
+import qualified Data.Map as M
+
 
 main = xmonad $ gnomeConfig
     { borderWidth           = 2
@@ -11,8 +13,19 @@ main = xmonad $ gnomeConfig
     , focusedBorderColor    = "#632d85"
     , manageHook            = myManageHook <+> manageHook gnomeConfig
     , layoutHook            = smartBorders $ layoutHook gnomeConfig
+    , keys                  = myKeys <+> keys gnomeConfig
     }
 
+
+myKeys (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask, xK_p), dmenuRun)
+    ]
+
+-- this assumes a properly patched dmenu capable of xft fonts is available
+dmenuRun = spawn "dmenu_run -i -b -fn 'Bitstream Vera Sans Mono-9' \
+                            \-nb '#1e2426' -nf '#babdb6' \
+                            \-sb '#632d85' -sf '#ffffff' \
+                            \-p 'run> '"
 
 myManageHook = composeOne [
                     isFullscreen -?> doFullFloat
